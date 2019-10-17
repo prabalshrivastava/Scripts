b2f569c0bde7bd62e2df47c39a7cc3473fced53a
prabal <prabalshrivastava54@gmail.com>
2019-10-17 16:44:03 +0530

    Added Environment Variables file

---
+++ setEnvironmentVariables.bat
@@ -0,0 +1,18 @@
+setx -m ANT_HOME "D:\Development\Program Files\apache-ant-1.9.13"
+setx -m GITBASH_HOME "D:\Development\Program Files\Git"
+setx -m TORTOISEGIT_HOME "D:\Development\Program Files\TortoiseGit"
+setx -m INTELLIJIDEA_HOME "D:\Development\Program Files\JetBrains\IntelliJ IDEA 2019.2.3"
+setx -m MAVEN_HOME "D:\Development\Program Files\apache-maven-3.6.2"
+setx -m GRADLE_HOME "D:\Development\Program Files\gradle-5.6.2"
+setx -m JDK_8 "D:\Development\Program Files\Java 8\jdk1.8.0_231"
+setx -m JDK_7 "D:\Development\Program Files\Java 7"
+setx -m ELASTICSEARCH_HOME "D:\Development\Program Files\elasticsearch-6.3.0"
+
+
+
+
+
+
+setx -m PATH "%PATH%;%ANT_HOME%\bin;%GITBASH_HOME%\cmd;%TORTOISEGIT_HOME%\bin;%INTELLIJIDEA_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%JDK_8%\bin;%JDK_7%\bin;%ELASTICSEARCH_HOME%\bin";
+
+pause
