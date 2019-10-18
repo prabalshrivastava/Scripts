1faa8c8a3ff54a1c75b0250a1aaedb0b415c99ef
prabal <prabalshrivastava54@gmail.com>
2019-10-18 12:36:23 +0530

    added extra variables

--- setEnvironmentVariables.bat
+++ setEnvironmentVariables.bat
@@ -5,14 +5,16 @@ setx -m INTELLIJIDEA_HOME "D:\Development\Program Files\JetBrains\IntelliJ IDEA 
 setx -m MAVEN_HOME "D:\Development\Program Files\apache-maven-3.6.2"
 setx -m GRADLE_HOME "D:\Development\Program Files\gradle-5.6.2"
 setx -m JDK_8 "D:\Development\Program Files\Java 8\jdk1.8.0_231"
-setx -m JDK_7 "D:\Development\Program Files\Java 7"
+setx -m JAVA_HOME "D:\Development\Program Files\Java 8\jdk1.8.0_231"
+setx -m JDK_7 "D:\Development\Program Files\Java 7\jdk1.7.0_80"
 setx -m ELASTICSEARCH_HOME "D:\Development\Program Files\elasticsearch-6.3.0"
+setx -m MYSQL_HOME "C:\Program Files\MySQL\MySQL Server 5.7"
 
 
 
 
 
 
-setx -m PATH "%PATH%;%ANT_HOME%\bin;%GITBASH_HOME%\cmd;%TORTOISEGIT_HOME%\bin;%INTELLIJIDEA_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%JDK_8%\bin;%JDK_7%\bin;%ELASTICSEARCH_HOME%\bin";
+setx -m PATH "%PATH%;%ANT_HOME%\bin;%GITBASH_HOME%\cmd;%TORTOISEGIT_HOME%\bin;%INTELLIJIDEA_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%JDK_8%\bin;%JDK_7%\bin;%ELASTICSEARCH_HOME%\bin;%MYSQL_HOME%\bin"
 
 pause
