89adf3dc23f8b26cdc6e8bf77df447b1f685bf76
prabal <prabalshrivastava54@gmail.com>
2019-10-23 12:05:56 +0530

    added User Account Center Setting to be disabled in DisableUAC.bat and logstash in setEnvironmentVariables.bat

--- setEnvironmentVariables.bat
+++ setEnvironmentVariables.bat
@@ -9,12 +9,12 @@ setx -m JAVA_HOME "D:\Development\Program Files\Java 8\jdk1.8.0_231"
 setx -m JDK_7 "D:\Development\Program Files\Java 7\jdk1.7.0_80"
 setx -m ELASTICSEARCH_HOME "D:\Development\Program Files\elasticsearch-6.3.0"
 setx -m MYSQL_HOME "C:\Program Files\MySQL\MySQL Server 5.7"
-setx -m KIBANA_HOME "D:\Development\Program Files\kibana-7.4.0-windows-x86_64"
+setx -m KIBANA_HOME "D:\Development\Program Files\elasticsearch-6.3.0"
+setx -m LOGSTASH_HOME "D:\Development\Program Files\logstash-6.3.0"
 
 
 
 
-
-setx -m PATH "%PATH%;%ANT_HOME%\bin;%GITBASH_HOME%\cmd;%TORTOISEGIT_HOME%\bin;%INTELLIJIDEA_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%JDK_8%\bin;%JDK_7%\bin;%ELASTICSEARCH_HOME%\bin;%MYSQL_HOME%\bin;%KIBANA_HOME%\bin"
+setx -m PATH "%PATH%;%ANT_HOME%\bin;%GITBASH_HOME%\cmd;%TORTOISEGIT_HOME%\bin;%INTELLIJIDEA_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%JDK_8%\bin;%JDK_7%\bin;%ELASTICSEARCH_HOME%\bin;%MYSQL_HOME%\bin;%KIBANA_HOME%\bin;%LOGSTASH_HOME%\bin"
 
 pause
