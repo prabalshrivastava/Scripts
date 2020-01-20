670dfc3c670dd697903109708cb625a31505a6da
mohasin.ccr <5hpac26dufowv7l6pei7ejozpdp6ipkmj5lcgubxng76zv6yf2nq>
2020-01-20 18:25:28 +0530

    added RunUI.bat

---
+++ RunUI.bat
@@ -0,0 +1,14 @@
+setx -m CODEBASE "D:\Development\Workspaces\Intellijidea Workspace1\Project CCRM\"
+
+cd "%CODEBASE%CCRM-MS-3-account-management-service\account-management-service-ui"
+start cmd /k npm start
+
+
+cd "%CODEBASE%CCRM-MS-4-open-distribution\open-distribution-ui
+start cmd /k npm start
+
+
+cd "%CODEBASE%CCRM-MS-4-open-distribution\open-distribution-cb-ui
+start cmd /k npm start
+
+pause
