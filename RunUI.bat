647bc55440c86f73b5b349962c52084cec5ced38
mohasin.ccr <5hpac26dufowv7l6pei7ejozpdp6ipkmj5lcgubxng76zv6yf2nq>
2020-01-23 09:29:58 +0530

    added code base path as a variable

--- RunUI.bat
+++ RunUI.bat
@@ -1,14 +1,16 @@
 setx -m CODEBASE "D:\Development\Workspaces\Intellijidea Workspace1\Project CCRM\"
 
+D:
+
 cd "%CODEBASE%CCRM-MS-3-account-management-service\account-management-service-ui"
 start cmd /k npm start
 
 
-cd "%CODEBASE%CCRM-MS-4-open-distribution\open-distribution-ui
+cd "%CODEBASE%CCRM-MS-4-open-distribution\open-distribution-ui"
 start cmd /k npm start
 
 
-cd "%CODEBASE%CCRM-MS-4-open-distribution\open-distribution-cb-ui
+cd "%CODEBASE%CCRM-MS-4-open-distribution\open-distribution-cb-ui"
 start cmd /k npm start
 
 pause
